f281bd82a745cc624aa8610ea9f1458845301ffe
Eslifrentz <99177319+Eslifrentz@users.noreply.github.com>
2022-03-07 14:00:08 +0100

    Add files via upload

---
+++ KNN good version.R
@@ -0,0 +1,83 @@
+--
+  title: "Assigment - kNN DIY"
+author:
+  - Author - Esli
+- Reviewer- Sander
+--
+
+datasets/KNN-hcvdat0.csv
+library(tidyverse)
+library(googlesheets4)
+library(class)
+library(caret)
+library(forcats)
+library(readr)
+
+                  
+#prepare data
+cleanDF <- rawDF[-1]
+head(cleanDF)
+
+cntDiag <- table(cleanDF$diagnosis)
+propDiag <- round(prop.table(cntDiag) * 100 , digits = 1)
+cntDiag
+
+propDiag
+
+cleanDF$diagnosis <- factor(cleanDF$diagnosis, levels = c("B", "M"), labels = c("Benign", "Malignant")) 
+
+head(cleanDF, 10)
+
+summary(cleanDF[c("radius_mean", "area_mean", "smoothness_mean")])
+
+normalize <- function(x) { # Function takes in a vector
+  return ((x - min(x)) / (max(x) - min(x))) # distance of item value - minimum vector value divided by the range of all vector values
+}
+
+#########
+## TEST ##
+#########
+
+testSet1 <- c(1:5)
+
+testSet2 <- c(1:5) * 10
+cat("testSet1:", testSet1, "\n")
+ 
+cat("testSet2:", testSet2, "\n")
+cat("Normalized testSet1:", normalize(testSet1), "\n")
+
+cat("Normalized testSet2:", normalize(testSet2))
+
+install.packages("magrittr") # package installations are only needed the first time you use it
+install.packages("dplyr")    # alternative installation of the %>%
+library(magrittr) # needs to be run every time you start R and want to use %>%
+library(dplyr)    # alternatively, this also loads %>%
+
+# normalize  --------------------------------------------------------------
+
+nCols <- dim(cleanDF)[2]
+cleanDF_n <- sapply(2:nCols,
+                    function(x) {
+                      normalize(cleanDF[,x])
+                    }) %>% as.data.frame()
+
+summary(cleanDF_n[c("radius_mean", "area_mean", "smoothness_mean")])
+summary(cleanDF_n[c("radius_mean", "area_mean", "smoothness_mean")])
+
+summary(cleanDF_n[c("radius_mean", "area_mean", "smoothness_mean")])
+
+### creating training sets
+trainDF_feat <- cleanDF_n[1:469,  ]
+testDF_feat <- cleanDF_n[470:569,  ]
+
+
+### seperating sets
+trainDF_labels <- cleanDF[1:469,  1]
+testDF_labels <- cleanDF[470:569,  1]
+
+### making matrix
+
+cleanDF_test_pred <- knn(train = as.matrix(trainDF_feat), test = as.matrix(testDF_feat), cl = as.matrix(trainDF_labels), k = 21)
+head(cleanDF_test_pred)
+
+confusionMatrix(cleanDF_test_pred, testDF_labels[[1]], positive = NULL, dnn = c("Prediction", "True"))
