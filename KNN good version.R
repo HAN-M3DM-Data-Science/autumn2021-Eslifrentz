82a73d679a01436145fa83bbab606c539771d2e9
Eslifrentz <99177319+Eslifrentz@users.noreply.github.com>
2022-03-07 22:19:31 +0100

    Add files via upload

--- KNN good version.R
+++ KNN good version.R
@@ -4,80 +4,98 @@ author:
   - Author - Esli
 - Reviewer- Sander
 --
-
-datasets/KNN-hcvdat0.csv
+install.packages("plotly")
+  
+  
+  ```{r}
 library(tidyverse)
 library(googlesheets4)
 library(class)
 library(caret)
-library(forcats)
-library(readr)
+library(magrittr)
+library(plotly)
 
-                  
-#prepare data
-cleanDF <- rawDF[-1]
+
+---
+  # Understanding the data
+  The data set contains laboratory values of blood donors, Hepatitis C patients and their demographic values.
+
+# Obtaining the data
+
+url <- "https://raw.githubusercontent.com/HAN-M3DM-Data-Mining/assignments/master/datasets/KNN-hcvdat0.csv"
+rawDF <- read_csv(url, )
+str(rawDF)
+
+
+# Preparating the data
+
+# Cleaning the data
+
+cleanDF <- na.omit(rawDF[-1])
 head(cleanDF)
 
-cntDiag <- table(cleanDF$diagnosis)
-propDiag <- round(prop.table(cntDiag) * 100 , digits = 1)
-cntDiag
 
-propDiag
+# Identifying the data (defined as either "Donor" or "Hepatitis")
 
-cleanDF$diagnosis <- factor(cleanDF$diagnosis, levels = c("B", "M"), labels = c("Benign", "Malignant")) 
+cleanDF$Category <- factor(cleanDF$Category)
+levels(cleanDF$Category) <- c("Donor", "Donor", "Hepatitis", "Hepatitis", "Hepatitis")
+levels(cleanDF$Category)
+table(cleanDF$Category)
 
-head(cleanDF, 10)
+# Visualizing the data
 
-summary(cleanDF[c("radius_mean", "area_mean", "smoothness_mean")])
+p1 <- ggplot(cleanDF) +
+  geom_point(aes(ALB, ALP, colour = Category))
+p1
 
-normalize <- function(x) { # Function takes in a vector
-  return ((x - min(x)) / (max(x) - min(x))) # distance of item value - minimum vector value divided by the range of all vector values
-}
 
-#########
-## TEST ##
-#########
+
+# Normalize numeric features
+# Setting a function 
+
+normalize <- function(x) 
+  return ((x - min(x)) / (max(x) - min(x)) 
+
+
+# Testing the function 
 
 testSet1 <- c(1:5)
-
 testSet2 <- c(1:5) * 10
 cat("testSet1:", testSet1, "\n")
- 
-cat("testSet2:", testSet2, "\n")
 cat("Normalized testSet1:", normalize(testSet1), "\n")
+cat("testSet2:", testSet2, "\n")
+cat("Normalized testSet2:", normalize(testSet2), "\n")
 
-cat("Normalized testSet2:", normalize(testSet2))
 
-install.packages("magrittr") # package installations are only needed the first time you use it
-install.packages("dplyr")    # alternative installation of the %>%
-library(magrittr) # needs to be run every time you start R and want to use %>%
-library(dplyr)    # alternatively, this also loads %>%
 
-# normalize  --------------------------------------------------------------
 
 nCols <- dim(cleanDF)[2]
-cleanDF_n <- sapply(2:nCols,
-                    function(x) {
-                      normalize(cleanDF[,x])
-                    }) %>% as.data.frame()
-
-summary(cleanDF_n[c("radius_mean", "area_mean", "smoothness_mean")])
-summary(cleanDF_n[c("radius_mean", "area_mean", "smoothness_mean")])
-
-summary(cleanDF_n[c("radius_mean", "area_mean", "smoothness_mean")])
-
-### creating training sets
-trainDF_feat <- cleanDF_n[1:469,  ]
-testDF_feat <- cleanDF_n[470:569,  ]
+cleanDFnorm <- sapply(4:nCols,
+                      function(x) {
+                        normalize(cleanDF[, x])
+                      }) %>% as.data.frame()
+head(cleanDFnorm)
 
 
-### seperating sets
-trainDF_labels <- cleanDF[1:469,  1]
-testDF_labels <- cleanDF[470:569,  1]
+# Preparing train and testing the datasets
 
-### making matrix
+s <- sample(c(1:dim(cleanDF)[1]), 469)
 
-cleanDF_test_pred <- knn(train = as.matrix(trainDF_feat), test = as.matrix(testDF_feat), cl = as.matrix(trainDF_labels), k = 21)
-head(cleanDF_test_pred)
+#Splitting training and testing data sets
+trainDFfeat <- cleanDFnorm[s,  ]
+testDFfeat <- cleanDFnorm[-s,  ]
 
-confusionMatrix(cleanDF_test_pred, testDF_labels[[1]], positive = NULL, dnn = c("Prediction", "True"))
+#Creating training and test data sets with labels
+trainDFlabels <- cleanDF[s, 1]
+testDFlabels <- cleanDF[-s, 1]
+
+
+
+# Modeling and evaluating
+
+cleanDFpred <- knn(train = as.matrix(trainDFfeat), test = as.matrix(testDFfeat), cl = as.matrix(trainDFlabels), k = 7)
+head(cleanDFpred)
+confusionMatrix(cleanDFpred, testDFlabels[[1]], positive = NULL, dnn = c("Prediction", "True"))
+dim(cleanDFpred)
+dim(testDFlabels)
+
